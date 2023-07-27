f68af6643433ed7323721949f8fc7eb198e3f60a
Shubham Gupta <sg319234@gmail.com>
2023-07-27 16:22:10 +0530

    login and register page created

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const app=express();
 
 const port=process.env.PORT || 3300;
@@ -9,11 +10,25 @@ app.use(express.static('assets'));
 
 app.set('view engine','ejs');
 // app.set('views',path.join(__dirname+'/assets'));
+app.use(expressEjsLayouts);
 
 
 app.get('/',(req,res)=>{
     res.render('home');
 })
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
+
 
 app.listen(port,(err)=>{
     if(err){
